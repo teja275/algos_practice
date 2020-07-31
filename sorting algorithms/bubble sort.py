182aadb306d10b49b2ff18d5dc2504588e78dcac
Bhanuteja Aryasomayajula <teja275@gmail.com>
2020-07-31 23:11:36 +0530

    added logic to break the loop if array is sorted after some passes

--- sorting algorithms/bubble sort.py
+++ sorting algorithms/bubble sort.py
@@ -1,8 +1,15 @@
 def bubble_sort(unsorted_array):
+    pass_cnt = 0
     for i in range(len(unsorted_array)-1, 0, -1):
+        check = 0
         for j in range(i):
             if unsorted_array[j] > unsorted_array[j+1]:
                 unsorted_array[j], unsorted_array[j + 1] = unsorted_array[j + 1], unsorted_array[j]
+                check += 1
+        if check == 0:
+                break
+        pass_cnt += 1
+    print(f"It took {pass_cnt} passes to sort the array")
     print(unsorted_array)
 
 
